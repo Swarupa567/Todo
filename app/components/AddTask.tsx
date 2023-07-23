362df1d19acc5901126f597047a23d40eea11040
Swarupa <swarupachowdaryp@gmail.com>
2023-07-23 09:54:03 +0530

    Create AddTask.tsx

---
+++ app/components/AddTask.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React, {useState, FormEventHandler} from 'react';
+import {AiOutlinePlus} from 'react-icons/ai';
+import Modal from './Modal';
+import { addTodo } from '@/api';
+import { useRouter } from 'next/navigation';
+import { v4 as uuidv4} from 'uuid';
+
+const AddTask = () => {
+    const router = useRouter();
+    const[modalOpen, setModalOpen] = useState<boolean>(false);
+    const [newTaskValue, setNewTaskValue] = useState<string>('');
+
+    const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = async (e) => {
+      e.preventDefault();
+      await addTodo({
+        id: uuidv4(),
+        text:newTaskValue
+      })
+      setNewTaskValue("");
+      setModalOpen(false);
+      router.refresh();
+    }
+
+    return <div>
+        <button onClick={() => setModalOpen(true)} className="btn btn-primary w-full">
+           Add new task<AiOutlinePlus size={16}/>
+        </button>
+        <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
+         <form onSubmit={handleSubmitNewTodo}>
+            <h3 className="font-bold text-lg">Add New Task</h3>
+            <div className="modal-action">
+               <input 
+               value={newTaskValue}
+               onChange={e => setNewTaskValue(e.target.value)}
+               type="text" 
+               placeholder="Type here"
+                className="input input-bordered w-full"
+                 />
+               <button className='btn' type='submit'>Submit</button>
+            </div>
+         </form>
+        </Modal>
+        </div>
+};
+
+export default AddTask;
